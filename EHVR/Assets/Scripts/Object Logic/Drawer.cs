9bf13856e93e9f271136d11be0c2c232dffcef7b
Jerry Ding <jerryding2022@berkeley.edu>
2019-12-10 14:23:37 -0800

    Perfect (I think) drawer physics

--- EHVR/Assets/Scripts/Object Logic/Drawer.cs
+++ EHVR/Assets/Scripts/Object Logic/Drawer.cs
@@ -13,14 +13,20 @@ public class Drawer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.transform.localPosition.x > 0.01922647f)
+        {
+            gameObject.transform.localPosition = new Vector3(0.01922647f, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+        } else if (gameObject.transform.localPosition.x < 0.01539536f)
+        {
+            gameObject.transform.localPosition = new Vector3(0.01539536f, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Shell"))
-        {
-            gameObject.tag = "Grabbable";
-        }
+        //if (other.gameObject.CompareTag("Shell"))
+        //{
+        //    gameObject.tag = "Grabbable";
+        //}
     }
 }
